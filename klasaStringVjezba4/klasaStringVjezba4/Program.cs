dcb598788daa4086538acfd706043eb3787857da
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-12-09 23:20:41 +0100

    Add files via upload

---
+++ klasaStringVjezba4/klasaStringVjezba4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite neku rečenicu:");
+        string rečenica = Console.ReadLine();
+
+        string[] riječi = rečenica.Split(' ');
+
+        if (riječi.Length > 0)
+        {
+            string prvaRiječ = riječi[0];
+            string zadnjaRiječ = riječi[riječi.Length - 1];
+
+            Console.WriteLine($"Prva riječ: {prvaRiječ}");
+            Console.WriteLine($"Zadnja riječ: {zadnjaRiječ}");
+        }
+        else
+        {
+            Console.WriteLine("Niste unijeli rečenicu.");
+            Console.ReadKey();
+        }
+    }
+}
